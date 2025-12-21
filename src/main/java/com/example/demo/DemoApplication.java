546b3353f1f0dc2593914c2ad4cd5e80ae9001ac
bala-amypo <bala@amypo.in>
2025-12-21 12:41:51 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-/**
- * Main entry point for the Digital Complaint Prioritization Engine
- */
 @SpringBootApplication
-@ServletComponentScan   // IMPORTANT: Enables @WebServlet scanning
+@ServletComponentScan
 public class DemoApplication {
 
     public static void main(String[] args) {
