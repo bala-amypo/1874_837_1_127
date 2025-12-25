8662de4d5cb29c6bc944119d8723cb1e639ce59a
bala-amypo <bala@amypo.in>
2025-12-25 15:46:38 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleEchoServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleEchoServlet.java
@@ -1,18 +1,21 @@
 package com.example.demo.servlet;
 
-import jakarta.servlet.http.*;
-import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class SimpleEchoServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws IOException {
 
         resp.setStatus(HttpServletResponse.SC_OK);
         resp.setContentType("text/plain");
 
         String name = req.getParameter("name");
+
         if (name == null || name.trim().isEmpty()) {
             resp.getWriter().write("Hello, Guest");
         } else {
